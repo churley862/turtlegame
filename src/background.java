124d1f5ead5f5d8c50cfd01bc346fefa6f883c39
Kitchen <kitchen@Kitchens-iMac.local>
2015-07-07 21:47:45 -0400

    Add import statements

--- src/background.java
+++ src/background.java
@@ -1,3 +1,12 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Image;
 
 public class background {
 
